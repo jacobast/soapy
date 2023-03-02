c560bc9eca2c7ce904e829c81a2f13235d653422
jacobast <astrand.jacob@gmail.com>
2023-03-02 18:57:28 +0100

    Firebase connection works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
+
+// Firebase
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebase),
+    //AngularFireAnalyticsModule,
+    AngularFirestoreModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDTfK8x-RHehzvZpNE793nxz4S-aflSDXo",
+    authDomain: "soapy-a783f.firebaseapp.com",
+    projectId: "soapy-a783f",
+    storageBucket: "soapy-a783f.appspot.com",
+    messagingSenderId: "1086132360270",
+    appId: "1:1086132360270:web:d24860505834dea851df1f"
+  }
+};
